6f75e4ab352d45018e79437386ed9a7bddb07923
Nitish Mahapatre <nitish468mahapatre@gmail.com>
2025-02-28 10:19:00 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -13,23 +13,27 @@ def power(x, y):
     return math.pow(x, y)
 
 if __name__ == "__main__":
-    print("Scientific Calculator")
-    print("1. Square Root (√x)")
-    print("2. Factorial (x!)")
-    print("3. Natural Log (ln(x))")
-    print("4. Power (xʸ)")
+    while True:
+        print("Scientific Calculator")
+        print("1. Square Root (√x)")
+        print("2. Factorial (x!)")
+        print("3. Natural Log (ln(x))")
+        print("4. Power (xʸ)")
+        print("5. Exit")
 
-    choice = int(input("Enter choice: "))
-    if choice in [1, 2, 3, 4]:
-        num = float(input("Enter number: "))
-        if choice == 1:
-            print("Result:", square_root(num))
-        elif choice == 2:
-            print("Result:", factorial(int(num)))
-        elif choice == 3:
-            print("Result:", natural_log(num))
-        elif choice == 4:
-            num2 = float(input("Enter exponent: "))
-            print("Result:", power(num, num2))
-    else:
-        print("Invalid choice")
+        choice = int(input("Enter choice: "))
+        if choice in [1, 2, 3, 4]:
+            num = float(input("Enter number: "))
+            if choice == 1:
+                print("Result:", square_root(num))
+            elif choice == 2:
+                print("Result:", factorial(int(num)))
+            elif choice == 3:
+                print("Result:", natural_log(num))
+            elif choice == 4:
+                num2 = float(input("Enter exponent: "))
+                print("Result:", power(num, num2))
+        elif choice==5:
+            break
+        else:
+            print("Invalid choice")
